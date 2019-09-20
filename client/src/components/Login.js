086dfcc4521465f949f5051e214d2300b1d1f3be
Derek Etman <dereketman@gmail.com>
2019-09-20 09:20:04 -0700

    +Login, redirect works

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,12 +1,59 @@
-import React from "react";
+import React, {useState} from "react";
+import { axiosWithAuth } from '../utils/axiosWithAuth';
 
-const Login = () => {
+const Login = (props) => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+  const [userLogin,setUserLogin] = useState({username:'', password:''})
+  console.log("USERLOGIN: ", userLogin);
+
+  const handleChange = e => {
+    console.log(e.target.value)
+    setUserLogin({
+      ...userLogin,
+      [e.target.name]: e.target.value
+    })
+  }
+  console.log("AxiosWithAuth", axiosWithAuth);
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    axiosWithAuth()
+    .post('/login', userLogin)
+    .then(res =>{
+      console.log(res); 
+      localStorage.setItem('token', res);      
+    })
+    .then(props.history.push('/bubblepage'))
+    .catch(err => console.log("Login Err: ", err))
+
+  }
+
+  
   return (
     <>
+    <form onSubmit={handleSubmit}>
       <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
+      <p>Please Login</p>
+
+      <input 
+      type="text" 
+      name="username"
+      placeholder="UserName"
+      value={userLogin.username}
+      onChange={handleChange}
+      />
+
+      <input
+      type="text"
+      name="password"
+      placeholder="Password"
+      value={userLogin.password}
+      onChange={handleChange}
+      />
+
+      <button>Log In</button>
+    </form>
     </>
   );
 };
